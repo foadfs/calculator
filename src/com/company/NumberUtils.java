16ed409b02a7a7c0575550c6091829c348cb994e
ahadi.masoud <masoud.ahadi@gmail.com>
2019-03-02 14:17:51 +0330

    Changes: rename result method to doCalculation.

--- src/com/company/NumberUtils.java
+++ src/com/company/NumberUtils.java
@@ -7,8 +7,9 @@ import java.io.*;
 public class NumberUtils {
     Scanner scanner;
     String path;
-    String operation="" ;
+    String operation = "";
     int firstNumber = 0, secondNumber = 0, result = 0;
+
     public void menu() {
         String option;
         System.out.println("menu:\n1-continue\n2-Exit");
@@ -18,9 +19,10 @@ public class NumberUtils {
             case "1":
                 createFile();
                 firstNumber = getNumber("enter first number:");
-                secondNumber= getNumber("enter second number:");
+                secondNumber = getNumber("enter second number:");
                 operation = getOperation();
-                operation="";
+                doCalculation(operation);
+                operation = "";
                 break;
             case "2":
                 System.exit(1);
@@ -36,7 +38,6 @@ public class NumberUtils {
                 !operation.equals("%")) {
             System.out.println("please enter your operation: ");
             operation = scanner.nextLine();
-            result(operation);
         }
         return operation;
     }
@@ -57,56 +58,59 @@ public class NumberUtils {
         }
         return a;
     }
-    private String messageOper(String messageOpera){
-        String mss=firstNumber+messageOpera+secondNumber+"="+result;
+
+    private String messageOper(String messageOpera) {
+        String mss = firstNumber + messageOpera + secondNumber + "=" + result;
         writeFile(mss);
         return mss;
     }
-    private String createFile(){
-        System.out.println("please enter your path file:\n (for example :c:\\data\\myfile.txt ) " );
+
+    private void createFile() {
+        System.out.println("please enter your path file:\n (for example :c:\\data\\myfile.txt ) ");
         path = scanner.nextLine();
-        return path;
     }
-    private void writeFile(String str){
+
+    private void writeFile(String str) {
         try {
-            File file= new File(path);
-            FileWriter fileWriter=new FileWriter(file,true);
+            File file = new File(path);
+            FileWriter fileWriter = new FileWriter(file, true);
             BufferedWriter out = new BufferedWriter(fileWriter);
             out.write(str);
             out.newLine();
             out.close();
             System.out.println("The file was successfully writed");
-        }catch (IOException e){
+        } catch (IOException e) {
             System.err.println("error!");
         }
     }
-    private int result(String operation){
+    private int doCalculation(String operation){
         switch (operation) {
             case "+":
-                result=firstNumber+secondNumber;
+                result = firstNumber + secondNumber;
                 System.out.println(messageOper("+"));
                 break;
             case "-":
-                result=firstNumber-secondNumber;
+                result = firstNumber - secondNumber;
                 System.out.println(messageOper("-"));
                 break;
             case "*":
-                result=firstNumber*secondNumber;
+                result = firstNumber * secondNumber;
                 System.out.println(messageOper("*"));
                 break;
             case "/":
                 try {
                     result = firstNumber / secondNumber;
                     System.out.println(messageOper("/"));
-                }catch (ArithmeticException e){
+                } catch (ArithmeticException e) {
                     System.err.println("you can not divided by zero! ");
                 }
                 break;
             case "%":
-                result=firstNumber%secondNumber;
+                result = firstNumber % secondNumber;
                 System.out.println(messageOper("%"));
             default:
-                System.err.println("\n Please enter only '/' or '*' or '-' or '+' or '%'"); }
+                System.err.println("\n Please enter only '/' or '*' or '-' or '+' or '%'");
+        }
         return result;
     }
 }
